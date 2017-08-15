ab077b4362666e81aac376c1ee6d12462497a5b7
Phanindra <phanindra.nitw@gmail.com>
2017-08-15 17:19:32 +0530

    use gcloud from bin

--- .circleci/deploy_prod.sh
+++ .circleci/deploy_prod.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${GOOGLE_PROJECT_ID}/inventory
+sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${GOOGLE_PROJECT_ID}/inventory:build-$CIRCLE_BUILD_NUM
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=gcr.io/${GOOGLE_PROJECT_ID}/inventory:build-$CIRCLE_BUILD_NUM
